f165634d68da92893032921ef4a0d3cb9c3f0427
polkaudio124 <ngchinhui88@gmail.com>
2021-02-14 18:49:06 +0800

    first

---
+++ src/test/java/com/ngch/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
